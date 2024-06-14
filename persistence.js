62292e969bdb71325cd5f60bb99079add3544a16
Alex Mendez <rmendezmartine00@mylangara.ca>
2024-06-14 11:37:06 -0700

    added id handling

--- persistence.js
+++ persistence.js
@@ -1,10 +1,16 @@
 let _notes = [
-    { id: "2", text: "CPSC 2650" },
-    { id: "1", text: "An awesome web dev Note" },
-  ];
-  
-  // TODO: implement addNote and removeNote
-  // For fun: why do we export a function instead of notes directly?
-  const notes = () => _notes;
-  
-  export { notes };
+  { id: "2", text: "CPSC 2650" },
+  { id: "1", text: "An awesome web dev Note" },
+];
+
+// TODO: implement addNote and removeNote
+// For fun: why do we export a function instead of notes directly?
+const notes = () => _notes;
+
+// Add a note
+const addNote = (note) => {
+  _notes.push(note);
+};
+
+
+export { notes, addNote, editNote, removeNote };
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import { notes } from '../persistence.js';
 
 const router = express.Router();
 
+
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Yay node!" });
+  const myNotes = notes();
+  res.render("index", { title: "YANT (Yet another Note Taking app)", notes: myNotes});
 });
 
 export default router;
